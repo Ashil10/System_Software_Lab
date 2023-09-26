d7cd755b9b0210e39d2d4ab704f4164769cf0859
Ashil <ashilkp2003@gmail.com>
2023-09-26 08:37:37 +0530

    Update lab_6.c

--- lab_6.c
+++ lab_6.c
@@ -58,5 +58,39 @@ int main() {
         printf("%s %d\n", symbolTable[i].label, symbolTable[i].address);
     }
 
+
+    Algorithm for Pass One of a Two-Pass Assembler:
+
+1. Initialize necessary data structures and variables:
+   - Open the input assembly file for reading.
+   - Initialize the location counter (LC) to 0.
+   - Initialize an empty symbol table.
+
+2. Read each line from the input assembly file until the end of the file:
+   a. Read a line from the input file.
+   b. Tokenize the line into label, opcode, and operand.
+
+3. Check if the opcode is "START":
+   a. If yes, set the LC to the value specified in the operand (the starting address).
+
+4. Check if there is a label:
+   a. If yes, add the label and its address (LC) to the symbol table.
+
+5. Check the opcode and update the LC accordingly:
+   a. If the opcode is an instruction that consumes memory (e.g., LDA, STA, ADD, SUB, etc.), increment LC accordingly.
+
+6. Check if the opcode is "END":
+   a. If yes, stop the pass one phase.
+
+7. Repeat steps 2-6 for each line in the input file.
+
+8. Close the input file.
+
+9. Print the symbol table, which now contains labels and their corresponding addresses.
+
+10. End of Pass One.
+
+
+
     return 0;
 }
